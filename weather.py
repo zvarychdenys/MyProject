17c537cfb8f58b00ab12195eaacb95cf9bf5090b
zvarychdenys <denys.zvr@gmail.com>
2021-07-12 16:03:04 +0200

    Added speed wind

--- weather.py
+++ weather.py
@@ -1,18 +1,27 @@
 from pyowm import OWM
 
-city = input("In which city do you want to find out the weather?")
+city = input("In which city do you want to find out the weather?\n")
 owm = OWM('37ace9ba0da02f3957cb68cae063f86b')  # free OWM API key
 mgr = owm.weather_manager()
 
-
-def foo(town):
-    observation = mgr.weather_at_place(town)
-    w = observation.weather
-    temperature = w.temperature('celsius')['temp']
-
-    return temperature
+observation = mgr.weather_at_place(city)
+w = observation.weather
 
 
-print(foo(city))
+def wind(town):
+    wn = w.wind()
+    speed_wind = wn['speed']
 
+    return speed_wind
+
+
+def temperature(town):
+
+    t = w.temperature('celsius')['temp']
+
+    return t
+
+
+print("In " + city + ", the temperature is " + str(temperature(city)) + " ℃")
+print("Speed wind in " + city + " is " + str(wind(city)) + " km/h")
 
